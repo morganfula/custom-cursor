275f68d1b5a753d2f571fd554085445551a89319
morganfula <morgan.fula@me.com>
2021-01-14 00:38:12 +0100

    done

--- cursor.js
+++ cursor.js
@@ -1,10 +1,16 @@
-const cursor = document.getElementById('cursor');
-
-console.log(cursor);
+const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', (e) => {
   cursor.setAttribute(
     'style',
-    'top: ' + e.pageY + 'px; left: ' + e.pageX + 'px'
+    'top: ' + (e.pageY - 10) + 'px; left: ' + (e.pageX - 10) + 'px;'
   );
+});
+
+document.addEventListener('click', () => {
+  cursor.classList.add('expand');
+
+  setTimeout(() => {
+    cursor.classList.remove('expand');
+  }, 500);
 });
